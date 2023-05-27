dffe174954fe65566ef4052621ad1ed29cffe605
Prashant <prashantkarki630@gmail.com>
2023-05-27 22:38:44 +0545

    searchable now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import Search from "./Components/Search";
+import "./Components/Search.css";
+import "./Components/utils.css";
+import SearchIcon from "@mui/icons-material/Search";
 import Feed from "./Components/Feed";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [movies, setMovies] = useState([]);
+  const API = "http://www.omdbapi.com/?apikey=83c91151&";
+
+  const getMovie = async (title) => {
+    const response = await fetch(`${API}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
   return (
     <div className="app">
-      <Search />
-      <Feed />
+      <div className="search flex ai-center">
+        <input
+          type="text"
+          value={search}
+          placeholder="Enter a movie"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <SearchIcon onClick={() => getMovie(search)} />
+      </div>
+      <Feed movies={movies} key={movies.Title} />
     </div>
   );
 }
--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,34 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './Feed.css'
 import './utils.css'
 
-function Feed() {
-    const [movies, setMovies] = useState([]);
-
-    const API = 'http://www.omdbapi.com/?apikey=83c91151&'
-
-    const getMovie = async (title) => {
-        // try {
-        const response = await fetch(`${API}&s={title}`);
-        // if (response.ok) {
-        const data = await response.json();
-        setMovies(data.Search)
-        //   } else {
-        //     const data = await response.json();
-        //     console.error('Error:', data);
-        //   }
-        // } catch (error) {
-        //   console.error("Error:", error.message)
-    }
-    // useEffect(() => {
-    //     getMovie('Spiderman');
-    // }, [])
+function Feed({ movies }) {
     return (
         <div className='feed flex jc-'>
             {
                 movies.map((movie) => {
                     return (
-
                         <div className='feed_card'>
                             <div className="title-wrapper flex fd-column">
                                 <h2>{movie.Title}</h2><br />
