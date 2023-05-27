ad0022b764c6a761cd93709ddf65f77262514874
Prashant <prashantkarki630@gmail.com>
2023-05-27 22:09:17 +0545

    fetchable now

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Feed.css'
+import './utils.css'
 
 function Feed() {
+    const [movies, setMovies] = useState([]);
+
+    const API = 'http://www.omdbapi.com/?apikey=83c91151&'
+
+    const getMovie = async (title) => {
+        // try {
+        const response = await fetch(`${API}&s={title}`);
+        // if (response.ok) {
+        const data = await response.json();
+        setMovies(data.Search)
+        //   } else {
+        //     const data = await response.json();
+        //     console.error('Error:', data);
+        //   }
+        // } catch (error) {
+        //   console.error("Error:", error.message)
+    }
+    // useEffect(() => {
+    //     getMovie('Spiderman');
+    // }, [])
     return (
-        <div className='feed'>
-            <div className='feed_card'>
-                <div className="title-wrapper flex fd-column jc-start">
-                    <h2>The Shawshank Redemption</h2><br />
-                    <p>1995</p><br />
-                </div>
-                <img src="https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1000_.jpg" alt="Poster" />
-                <button>Play</button>
-            </div>
+        <div className='feed flex jc-'>
+            {
+                movies.map((movie) => {
+                    return (
+
+                        <div className='feed_card'>
+                            <div className="title-wrapper flex fd-column">
+                                <h2>{movie.Title}</h2><br />
+                                <p>{movie.Year}</p><br />
+                            </div>
+                            <img src={movie.Poster} alt="Poster" />
+                            <button>Play</button>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -6,24 +6,10 @@ import SearchIcon from '@mui/icons-material/Search';
 function Search() {
   const [search, setSearch] = useState('')
 
-  const API = 'http://www.omdbapi.com?&apikey=83c91151'
-
-  const getMovie = async (title) => {
-    const response = await fetch(`${API}`);
-    const data = await response.json();
-    console.log(data.Search)
-  }
-
-  useEffect(() => {
-    getMovie();
-  }, [])
-
-
-
   return (
-    <div className='search flex ai-center jc-center'>
+    <div className='search flex ai-center'>
       <input type="text" value={search} placeholder='Enter a movie' onChange={(e) => setSearch(e.target.value)} />
-      <SearchIcon />
+      <SearchIcon  />
     </div >
   )
 }
